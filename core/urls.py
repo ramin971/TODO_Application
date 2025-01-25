d20ac4fedfe2addfb3022df32e67aa32e70a9509
ramin rahmani <ramin.rahmani971@gmail.com>
2025-01-25 18:30:02 +0330

    start TODO_project....

---
+++ core/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path,include
+from rest_framework import routers
+# from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
+# from .views import
+
+
+
+
+router = routers.SimpleRouter()
+# router.register(r'users', UserViewSet)
+
+
+
+urlpatterns = [
+    # path('auth/', include('auth_app.urls')),
+    # path('',include(router.urls)),
+
+    # Swagger
+    # path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+   
+
+    # path(),
+]
